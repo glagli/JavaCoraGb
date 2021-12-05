d61033ec3c9027a4c3e38b344cf0bf83e34f314d
Vladislav <Glagli@yandex.ru>
2021-12-05 17:57:20 +0300

    add lesson3

---
+++ src/main/java/Apple.java
@@ -0,0 +1,6 @@
+public class Apple extends Fruit{
+
+    public Apple(int count, String colour, double weight) {
+        super(count, colour, weight);
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,25 +1,80 @@
 public class main {
     public static void main(String[] args) {
-        Participants participants1 = new Participants("Trevor","Filips",45,80,85);
-        Participants participants2 = new Participants("Maikel","Taunli",50,45,50);
-        Participants participants3 = new Participants("Franklin","Klinton",25,85,90);
-        Participants participants4 = new Participants("Karl","Jonson",30,80,70);
-        Participants participants5 = new Participants("Niko","Belick",43,66,66);
-        Participants participants6 = new Participants("Klod","Smith",26,87,70);
-        Participants participants7 = new Participants("Tommi","Versety",30,74,70);
-        Participants participants8 = new Participants("David","Ueston",40,88,70);
 
-        Team team1 = new Team("New Mans",new Participants[]{participants6,participants8,participants3,participants4});
-        Team team2 = new Team("Old Mans",new Participants[]{participants1,participants2,participants5,participants7});
-        team1.infoTeam();
-        System.out.println("---------------------------------");
-        team2.infoTeam();
-        System.out.println("---------------------------------");
-        Course course = new Course();
-        course.doIt(team1);
-        course.doIt(team2);
-        team1.showResults(course);
-        System.out.println("---------------------------------");
-        team2.showResults(course);
+        System.out.println("Задание 1");
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 22, 11, 34, 54, 34, 43};
+        swapArr(arr, 1, 5);
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Задание 2");
+        Apple apples = new Apple(18, "red", 1);
+        Apple apples2 = new Apple(10, "red", 1);
+        Orange oranges = new Orange(12, "orange", 1.5);
+        Orange oranges2 = new Orange(2, "orange", 1.5);
+
+
+        Box<Apple> box1 = new Box<>(apples, "Коробка с яблоками №1 ");
+        Box<Orange> box2 = new Box<>(oranges, "Коробка с апельсинами №1 ");
+        Box<Apple> box3 = new Box<>(apples2, "Коробка с яблоками №2 ");
+
+        System.out.println(box1.showCount());
+        System.out.println(box2.showCount());
+        System.out.println(box3.showCount());
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Масса коробки №1 c яблоками " + "= " + box1.getWeight());
+        System.out.println("Масса коробки №1 c апельсинами " + "= " + box2.getWeight());
+        System.out.println("Масса коробки №2 c яблоками " + "= " + box3.getWeight());
+        System.out.println("-------------------------------------------------");
+
+        System.out.print("Сравним коробку с яблоками №1 и коробку с апельсинами №1 : ");
+        System.out.println(box1.compare(box2));
+        System.out.print("Сравним коробку с яблоками №1 и коробку с яблоками №2 : ");
+        System.out.println(box1.compare(box3));
+        System.out.println("-------------------------------------------------");
+
+
+        System.out.println("Пересыпем яблоки из коробки №1 в коробку с яблоками №2: ");
+        box1.pourOver(box3);
+        System.out.println(box1.showCount());
+        System.out.println(box3.showCount());
+        //box1.pourOver(box2); // Корзину с яблоками нельзя пересыпать в корзину с апельсинами, как и задумано
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Добавим в корзину с апельсинов в корзину");
+        System.out.print("Перед добавлением: ");
+        System.out.println(box2.showCount());
+        box2.addFruit(oranges2);
+        System.out.print("После добавления: ");
+        System.out.println(box2.showCount());
+        // box2.addFruit(apples2); // В корзину с апельсинами нельзя добавить яблок, как и задумано
+
+
     }
+
+    public static void swapArr(Integer[] arr, int one, int two) {
+        try {
+            System.out.print("Исх. массив: ");
+            for (Integer integer : arr) {
+                System.out.print(integer + " ");
+            }
+            System.out.println("");
+            one = one - 1;
+            two = two - 1;
+            int container;
+            container = arr[one];
+            arr[one] = arr[two];
+            arr[two] = container;
+            System.out.print("Изм. массив: ");
+            for (Integer integer : arr) {
+                System.out.print(integer + " ");
+            }
+            System.out.println("");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Введите индекс элемента внутри массива");
+            System.out.println("Массив состоит из " + arr.length + " элементов\n");
+        }
+    }
+
+
 }
